49d6a774d2ee56c45685c07b00b806b7789430b7
Bryan Santos <brymsantos@gmail.com>
2022-06-21 13:16:54 -0700

    worked on numIslands again

--- NeetCode/Graphs/numIslands.js
+++ NeetCode/Graphs/numIslands.js
@@ -18,33 +18,93 @@ Output: 1
  * @return {number}
  */
 var numIslands = function (grid) {
-  const H = grid.length;
+  let res = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === '0') continue;
+      res++;
+      searchAdjacent(i, j);
+    }
+  }
+  function searchAdjacent(i, j) {
+    if (i < 0 || j < 0 || i === grid.length || j === grid[0].length) return;
+    if (grid[i][j] === '0') return;
+    grid[i][j] = '0';
+    searchAdjacent(i + 1, j);
+    searchAdjacent(i - 1, j);
+    searchAdjacent(i, j - 1);
+    searchAdjacent(i, j + 1);
+  }
+  return res;
+};
+grid = [
+  ['1', '1', '1', '1', '0'],
+  ['1', '1', '0', '1', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '0', '0', '0'],
+];
+console.log(numIslands(grid));
+
+/**
+ *       const H = grid.length;
   const W = grid[0].length;
   let count = 0;
+  
   for (let r = 0; r < H; r++) {
     for (let c = 0; c < W; c++) {
       if (grid[r][c] === '0') continue;
-
+      
       count++;
       dfs(r, c);
     }
   }
   return count;
-
+  
   function dfs(r, c) {
-    // if you hit out of bounds or water return
     if (r < 0 || c < 0 || r === H || c === W) return;
     if (grid[r][c] === '0') return;
-
-    // mark the spot that was visited
+    
     grid[r][c] = '0';
-    // check up
-    dfs(r - 1, c);
-    // check down
-    dfs(r + 1, c);
-    // check left
-    dfs(r, c - 1);
-    // check right
-    dfs(r, c + 1);
+    dfs(r-1, c);
+    dfs(r+1, c);
+    dfs(r, c-1);
+    dfs(r, c+1);
   }
-};
+    
+ */
+
+/**
+ * BFS approach
+ *   let num = 0;
+  let r = grid.length;
+  let c = grid[0].length;
+  // iterate through each element of the grid,
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === '0') continue;
+      num++;
+      const queue = [[i, j]];
+      while (queue.length) {
+        console.log('queue', queue);
+        const [i, j] = queue.shift();
+        console.log(i, j);
+        grid[i][j] = '0';
+        if (i - 1 >= 0) {
+          if (grid[i - 1][j] === '1') {
+            queue.push([i - 1, j]);
+          }
+        }
+        if (i + 1 < grid.length) {
+          if (grid[i + 1][j] === '1') queue.push([i + 1, j]);
+        }
+        if (j - 1 >= 0) {
+          if (grid[i][j - 1] === '1') queue.push([i, j - 1]);
+        }
+        if (j + 1 < grid[0].length) {
+          if (grid[i][j + 1] === '1') queue.push([i, j + 1]);
+        }
+      }
+    }
+  }
+  return num;
+ */
