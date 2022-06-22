058e263e519424c645d1181ca6cd13864194a90b
Bryan Santos <brymsantos@gmail.com>
2022-06-21 17:46:57 -0700

    solved maxAreaOfIsland

---
+++ NeetCode/Graphs/maxAreaofIsland.js
@@ -0,0 +1,61 @@
+/**
+ * You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+ */
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function (grid) {
+  let maxArea = 0;
+  // traverse through each element
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 0) continue;
+      maxArea = Math.max(traverse(i, j), maxArea);
+    }
+  }
+
+  return maxArea;
+
+  function traverse(i, j, counter = 0) {
+    if (i < 0 || i === grid.length || j < 0 || j === grid[0].length)
+      return counter;
+    if (grid[i][j] === 0) return counter;
+    grid[i][j] = 0;
+    counter++;
+    counter = traverse(i - 1, j, counter);
+    counter = traverse(i + 1, j, counter);
+    counter = traverse(i, j - 1, counter);
+    return traverse(i, j + 1, counter);
+  }
+};
+
+grid = [
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+];
+
+console.log(maxAreaOfIsland(grid));
