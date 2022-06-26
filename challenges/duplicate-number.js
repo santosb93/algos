4724580bb0491cefaa9b90fefcaee6d6f21d1768
Bryan Santos <brymsantos@gmail.com>
2022-06-26 15:54:43 -0700

    worked on duplicate-number

--- challenges/duplicate-number.js
+++ challenges/duplicate-number.js
@@ -16,6 +16,15 @@ const duplicateNumber = (array) => {
 You have an unsorted array of n + 1 numbers, with the range of k to (k + n - 1), with an extra number that is a duplicate.
 (k is not necessarily 1) Find the duplicate. Do this in O(n) time and O(1) space complexity.
 ex: [3, 4, 7, 6, 8, 5, 6] should return 6
+
+[ 3 , 4 , 5 , 6 , 6 , 7 , 8]
+n + 1 numbers 
+n = 6
+min = 3
+max < 9
+
+3 , 4, 5 , 6 , 7, 8 , 
+
 */
 
 // n is 6
